51d6b0e0457c21715767252db49fd47f1bc407d7
webseon@gmail.com <dreamFActory3!>
2021-05-10 14:44:17 +0900

    #21 처리중

--- src/editor/DataItem.js
+++ src/editor/DataItem.js
@@ -8,7 +8,7 @@ function DataItem() {
     deg: 90,
     visible: true,
     position: {x: 0, xUnit: '%', y: 0, yUnit: '%'},
-    size: {w: 30, wUnit: '%', h: 30, hUnit: '%'},
+    size: {w: 0, wUnit: '%', h: 0, hUnit: '%'},
     colorList: [
       {color: 'rgba(255,255,255,1)', rangeUnit:'%',range: 0},
       {color: 'rgba(255,255,255,0.1)',rangeUnit:'%',range: 100}
