c7eb9d1c8caeb7e7c76f677f1621af5fcc1f9d4a
webseon@gmail.com <dreamFActory3!>
2021-05-12 18:12:45 +0900

    - 음냥..

--- src/editor/DataColor.js
+++ src/editor/DataColor.js
@@ -1,5 +1,5 @@
-function DataColor(color = 'rgba(255,255,255,1)', range = 0, rangeUnit = '%', useDivide = false) {
-  return {color: color, rangeUnit: rangeUnit, range: range, useDivide: useDivide};
+function DataColor(color = 'rgba(255,255,255,1)', range = 0, rangeUnit = '%', useDivide = false, useDivideEnd = false) {
+  return {color: color, colorEnd : color,rangeUnit: rangeUnit, range: range, useDivide: useDivide, useDivideEnd: useDivideEnd};
 }
 
 export default DataColor;
