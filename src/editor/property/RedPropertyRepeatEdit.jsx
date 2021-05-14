f497aac874a71ad0f5c0189b825850033ea713d6
webseon@gmail.com <dreamFActory3!>
2021-05-14 15:32:55 +0900

    리피트모드의 버튼화 #38

--- src/editor/property/RedPropertyRepeatEdit.jsx
+++ src/editor/property/RedPropertyRepeatEdit.jsx
@@ -11,17 +11,123 @@ class RedPropertyRepeatEdit extends React.Component {
     const rootComponent = this.props.rootComponent;
     const rootComponentState = rootComponent.state;
     const activeSubData = rootComponentState.activeSubData;
+    const gradient = 'radial-gradient(at 50% 50%, rgba(255, 255, 0, 0.9) 1.5px, rgba(255, 0, 0, 0.5) 5px, transparent 10px)';
     return <div>
       Repeat Mode
-      <RedSelect
-        value={activeSubData['typeRepeat']}
-        options={Object.entries(REPEAT_TYPE)}
-        HD_change={e => {
-          activeSubData['typeRepeat'] = e.target.value;
-          rootComponent.setState({});
-        }} />
+      <div style={{display: 'flex',marginTop : '3px'}}>
+        <div style={{display: 'flex'}}>
+          <div style={{...style.icon}}
+               onClick={e => {
+                 activeSubData['typeRepeat'] = 'no-repeat';
+                 rootComponent.setState({});
+               }}
+          >
+            <div style={{
+              ...style.baseBg,
+              background: gradient,
+              backgroundSize: '10px 10px',
+              backgroundRepeat: 'no-repeat'
+            }}
+            />
+          </div>
+          <div style={{...style.icon}}
+               onClick={e => {
+                 activeSubData['typeRepeat'] = 'repeat-x';
+                 rootComponent.setState({});
+               }}
+          >
+            <div style={{
+              ...style.baseBg,
+              background: gradient,
+              backgroundSize: '10px 10px',
+              backgroundRepeat: 'repeat-x'
+            }}
+            />
+          </div>
+          <div style={{...style.icon}}
+               onClick={e => {
+                 activeSubData['typeRepeat'] = 'repeat-y';
+                 rootComponent.setState({});
+               }}
+          >
+            <div style={{
+              ...style.baseBg,
+              background: gradient,
+              backgroundSize: '10px 10px',
+              backgroundRepeat: 'repeat-y'
+            }}
+            />
+          </div>
+          <div style={{...style.icon}}
+               onClick={e => {
+                 activeSubData['typeRepeat'] = 'repeat';
+                 rootComponent.setState({});
+               }}
+          >
+            <div style={{
+              ...style.baseBg,
+              background: gradient,
+              backgroundSize: '10px 10px',
+              backgroundRepeat: 'repeat'
+            }}
+            />
+          </div>
+          <div style={{...style.icon}}
+               onClick={e => {
+                 activeSubData['typeRepeat'] = 'space';
+                 rootComponent.setState({});
+               }}
+          >
+            <div style={{
+              ...style.baseBg,
+              background: gradient,
+              backgroundSize: '10px 10px',
+              backgroundRepeat: 'space'
+            }}
+            />
+          </div>
+          <div style={{...style.icon}}
+               onClick={e => {
+                 activeSubData['typeRepeat'] = 'round';
+                 rootComponent.setState({});
+               }}
+          >
+            <div style={{
+              ...style.baseBg,
+              background: gradient,
+              backgroundSize: '10px 10px',
+              backgroundRepeat: 'round'
+            }}
+            />
+          </div>
+        </div>
+        <RedSelect
+          value={activeSubData['typeRepeat']}
+          options={Object.entries(REPEAT_TYPE)}
+          HD_change={e => {
+            activeSubData['typeRepeat'] = e.target.value;
+            rootComponent.setState({});
+          }} />
+      </div>
     </div>;
   }
 }
 
 export default RedPropertyRepeatEdit;
+const style = {
+  icon: {
+    width: '30px',
+    height: '30px',
+    borderRadius: '5px',
+    border: '1px solid #000',
+    overflow: 'hidden',
+    marginRight: '5px',
+    cursor : 'pointer'
+  },
+  baseBg: {
+    position: 'absolute',
+    top: '50%', left: '50%', transform: 'translate(-50%,-50%)',
+    width: '100%', height: '100%',
+
+  }
+};
