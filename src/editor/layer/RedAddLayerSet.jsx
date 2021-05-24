8f5033cf6523c0801d235a883492f9c70d95017f
webseon@gmail.com <dreamFActory3!>
2021-05-24 09:38:39 +0900

    #73 - 연구중

--- src/editor/layer/RedAddLayerSet.jsx
+++ src/editor/layer/RedAddLayerSet.jsx
@@ -11,6 +11,9 @@ import gsap from "gsap";
 import RedSelect from "../../core/RedSelect.jsx";
 import GRADIENT_TYPE from "../GRADIENT_TYPE.js";
 import DataColor from "../data/DataColor.js";
+import {ColorPicker} from "@easylogic/colorpicker";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faThumbtack} from "@fortawesome/free-solid-svg-icons";
 
 const easeNameList = [
   'none',
@@ -28,15 +31,23 @@ const easeTypeList = [
   'out',
   'inOut'
 ]
-
+let targetColorData
 class RedAddLayerSet extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       division: 5,
       easeName: 'none',
-      easeType: 'out'
+      easeType: 'out',
+      tempColorList : [],
+      colorPicker: [],
+      openColorPicker : []
     };
+    this.refColorPickerContainer = []
+    let  i = 20
+    while(i--){
+      this.refColorPickerContainer[i] = React.createRef()
+    }
   }
 
   renderGradientColorList(colorList) {
@@ -56,14 +67,14 @@ class RedAddLayerSet extends React.Component {
       }
     });
     const code = `${GRADIENT_TYPE.LINEAR}(90deg, ${gradients})`;
-    console.log(code)
+    // console.log(code)
     return code;
   };
 
   render() {
     let rangeList = new Array(this.state.division)
     rangeList.fill(1)
-    let tempColorList = []
+    const tempColorList = this.state.tempColorList
     return <div style={style.bg}>
       <div style={style.container}>
         TODO - 레이어셋 처리
@@ -84,7 +95,7 @@ class RedAddLayerSet extends React.Component {
             this.setState({easeType: e.target.value})
           }}/>
         </div>
-        <div style={{display: 'flex', justiceContent: 'space-between'}}>
+        <div style={{display: 'flex', justiceContent: 'space-between',width: '100%'}}>
           {
             rangeList.map((v, index) => {
               let ease = this.state.easeName === 'none' ? 'none' : `${this.state.easeName}.${this.state.easeType}`
@@ -96,14 +107,65 @@ class RedAddLayerSet extends React.Component {
               let tETween = gsap.to(tS, {value: 100, duration: 1, ease: ease});
               tETween.pause()
               tETween.seek((index + 1) / rangeList.length)
-              console.log(tSTween)
+              // console.log(tSTween)
               tS = tSTween.ratio * 100
               tE = tETween.ratio * 100
               let t0 = index / rangeList.length /2 +0.5
-              tempColorList.push(new DataColor(`rgba(255,${255 * t0},${255-255 * t0},1)`, tS, '%', false, false, true, tE))
-              return <div style={{padding: '5px', border: '1px solid red'}}>
+              let tColor = tempColorList[index] ? tempColorList[index]['color'] : `rgba(255,${255 * t0},${255-255 * t0},1)`
+              tempColorList[index] = tempColorList[index] || (new DataColor(tColor, tS, '%', false, false, true, tE))
+              tempColorList[index]['range'] = tS
+              tempColorList[index]['rangeEnd'] = tE
+              tempColorList[index]['colorEnd'] = tempColorList[index]['color']
+              const tColorData = tempColorList[index]
+              return <div style={{position:'relative',padding: '5px', border: '1px solid red',width: `${100/rangeList.length}%`,}}>
                 <div>{tS.toFixed()}% ~</div>
                 <div>{tE.toFixed()}%</div>
+                <div
+                  style={{
+                    width: '28px', height: '28px', margin: '1px',
+                    textAlign: 'center',
+                  }}
+                  className={'transparent_checker'}
+                >
+                  <div style={{
+                    background: tColor,
+                    width: '28px', height: '28px',
+                    borderRadius: '4px', border: '1px solid #000',
+                    marginRight: '10px',
+                    textAlign: 'center',
+                  }}
+                       onClick={() => {
+                         if (!this.state.colorPicker[index]) {
+                           this.state.colorPicker[index] = new ColorPicker({
+                             type: "sketch",
+                             position: 'inline',
+                             color: tColor,
+                             container: this.refColorPickerContainer[index].current,
+                             onChange: color => {
+                               tColorData['color'] = color;
+                               this.setState({})
+                             }
+                           });
+                         }
+                         targetColorData = tColorData
+                         // this.state.colorPicker[index].setOption({color: tColor});
+                         this.state.colorPicker[index].initColorWithoutChangeEvent(tColor);
+                         this.state.openColorPicker= []
+                         this.state.openColorPicker[index] = true
+                         this.setState({});
+                       }}
+                  />
+
+                </div>
+
+
+                <div style={{...style.colorPicker, display: this.state.openColorPicker[index] ? 'block' : 'none'}}>
+                  <div ref={this.refColorPickerContainer[index]}/>
+                  <div style={style.complete} onClick={() => {
+                    this.state.openColorPicker[index] = false
+                    this.setState({})
+                  }}>완료</div>
+                </div>
               </div>
             })
           }
@@ -115,12 +177,12 @@ class RedAddLayerSet extends React.Component {
           <div>Todo - 그라디언트 프리뷰</div>
           <div style={{
             height: '52px',
-
             background: this.renderGradientColorList(tempColorList)
           }}>
-            {tempColorList.map(v=>{
+            {tempColorList.map((v,index)=>{
+              if(index>=rangeList.length) return ''
               return <div style={{position :'absolute',
-                border : '1px dashed rgba(255,255,255,0.5)',
+                borderRight : '1px dashed rgba(0,0,0,1)',
                 top : 0,
                 left: v['range']+'%',
                 width : (v['rangeEnd']-v['range'])+'%',
@@ -150,7 +212,7 @@ const style = {
     position: 'fixed',
     top: 0, left: 0, right: 0, bottom: 0,
     background: 'rgba(0,0,0,0.76)',
-    zIndex: 1
+    zIndex: 2
   },
   container: {
     position: 'fixed',
@@ -168,5 +230,13 @@ const style = {
     justifyContent: 'flex-start',
     alignItems: 'center',
     flexDirection: 'column'
-  }
+  },
+  colorPicker: {
+    zIndex: 1, position: 'absolute', top: 0, left: '50%', transform: 'translate(-50% , 150px)',
+    boxShadow: '0px 0px 16px rgba(0,0,0,0.5)',
+    background: '#fff',
+    borderRadius: '8px',
+    overflow: 'hidden'
+  },
+  complete: {padding: '4px', background: '#5e7ade', cursor: 'pointer', textAlign: 'center'},
 };
